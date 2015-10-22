8836c321adcadab0782fbed44005d0de8e759bce
Jon-Erik <jonerik@pdx.edu>
2015-10-22 11:09:18 -0700

    Added Push Notification from Doc Sent -> Mirror

--- calendar.php
+++ calendar.php
@@ -2,6 +2,8 @@
 	
 	// Set the url of the calendar feed.
 	//$url = 'https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M';
+	//$url = 'https://www.google.com/calendar/ical/joneriksvenson%40gmail.com/public/basic.ics';
+	$url = 'https://www.google.com/calendar/ical/joneriksvenson%40gmail.com/private-604bf68373c5c44b252917d549964315/basic.ics';
 
 	/*****************************************/
 
